9894524141bfa5cb0895ed43218f8391ade0b0f4
ben <ben@mbp-de-benjamin>
2015-07-27 13:23:20 +0200

    Saisie temps : lot 1 = affichage liste tâche du jour

--- src/main/resources/public/js/timesheet/timesheet.module.js
+++ src/main/resources/public/js/timesheet/timesheet.module.js
@@ -5,6 +5,16 @@
         vm.dateTS = dateTS;
         vm.tasks = taskResource.userDay({day: dateTS.getTime()});
         vm.assignation = {};
+        vm.submit = function() {
+            var keys = Object.keys(vm.assignation);
+            var assignationSum = 0;
+            for (var i = 0; i < keys.length; i++) {
+                assignationSum += vm.assignation[keys[i]].timespend;
+            }
+            if (assignationSum > 10) {
+                vm.formError = "Les temps saisis sur une journée ne peut excéder 10";
+            }
+        };
     }
 
     timesheetController.$inject = ["taskResource", "dateTS"];
