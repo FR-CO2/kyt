efaceb33a7492432f585d316041e02ee92766b16
stanislas GRANEL <dvalin@dvalin.info>
2015-07-29 19:38:13 +0200

    Modification afin d'ajouter une tache pour une journée.

--- src/main/resources/public/js/timesheet/timesheet.module.js
+++ src/main/resources/public/js/timesheet/timesheet.module.js
@@ -4,6 +4,14 @@
         var vm = this;
         vm.dateTS = dateTS;
         vm.tasks = taskResource.userDay({day: dateTS.getTime()});
+        vm.loadOthersUserTask = function() {
+            vm.othersTask = taskResource.user();
+        };
+        
+        vm.addTask =function(task){
+            console.log("test");
+            vm.tasks.push(task);
+        };
         vm.assignation = {};
     }
 
