e196a5f4a4163827652a97d6320930b9c3f7f150
ben <ben@192.168.3.8>
2015-12-15 19:25:19 +0100

    Refactor

--- src/main/resources/public/scripts/app/project/kanban/kanban.controller.js
+++ src/main/resources/public/scripts/app/project/kanban/kanban.controller.js
@@ -5,7 +5,7 @@
             var loadKanban = function () {
                 vm.states = project.resource("states").query({"order": "position"});
                 vm.swimlanes = kanbanService.load(project);
-            }
+            };
             project.$promise.then(loadKanban);
             vm.addTask = function () {
                 var modalInstance = $modal.open({
