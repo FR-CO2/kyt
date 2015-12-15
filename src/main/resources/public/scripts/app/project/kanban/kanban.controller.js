ce4c0d39c3f49afcbcda2b3aa4cf5a7ba3b39e33
ben <ben@192.168.3.8>
2015-12-15 19:25:10 +0100

    Refactor

--- src/main/resources/public/scripts/app/project/kanban/kanban.controller.js
+++ src/main/resources/public/scripts/app/project/kanban/kanban.controller.js
@@ -2,27 +2,26 @@
     define([], function () {
         var kanbanController = function ($modal, project, kanbanService) {
             var vm = this;
-            project.$promise.then(function () {
+            var loadKanban = function () {
                 vm.states = project.resource("states").query({"order": "position"});
                 vm.swimlanes = kanbanService.load(project);
-            });
+            }
+            project.$promise.then(loadKanban);
             vm.addTask = function () {
                 var modalInstance = $modal.open({
                     animation: true,
                     templateUrl: "templates/project/task/add.html",
                     controller: "addTaskController",
                     controllerAs: "addTaskCtrl",
-                    resolve : {
-                        project : function () {
+                    resolve: {
+                        project: function () {
                             return project;
                         }
                     },
                     size: "md"
                 });
-                modalInstance.result.then(function () {
-                    vm.swimlanes = kanbanService.load(project);
-                });
-            }
+                modalInstance.result.then(loadKanban);
+            };
         };
         kanbanController.$inject = ["$modal", "project", "kanbanService"];
         return kanbanController;
