685ae52ee5c60d87b585bf102dfc647774c44291
ben <ben@mbp-de-benjamin>
2015-12-16 16:11:20 +0100

    Refactor

--- src/main/resources/public/scripts/app/project/task/list.controller.js
+++ src/main/resources/public/scripts/app/project/task/list.controller.js
@@ -1,12 +1,31 @@
 (function () {
     define(["angular"], function (angular) {
-        var taskController = function (project, taskAssemblerService) {
+        var taskController = function ($uibModal, project, taskAssemblerService) {
             var vm = this;
-            vm.tasks = project.resource("task", "page").get(function() {
+            vm.tasks = project.resource("task", "page").get(function () {
                 return angular.forEach(vm.tasks._embedded.taskResourceList, taskAssemblerService);
             });
+            vm.addTask = function () {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: "templates/project/task/add.html",
+                    controller: "addTaskController",
+                    controllerAs: "addTaskCtrl",
+                    resolve: {
+                        project: function () {
+                            return project;
+                        }
+                    },
+                    size: "md"
+                });
+                modalInstance.result.then(function () {
+                    vm.tasks = project.resource("task", "page").get(function () {
+                        return angular.forEach(vm.tasks._embedded.taskResourceList, taskAssemblerService);
+                    });
+                });
+            };
         };
-        taskController.$inject = ["project", "taskAssemblerService"];
+        taskController.$inject = ["$uibModal", "project", "taskAssemblerService"];
         return taskController;
     });
 })();
