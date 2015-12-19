180eaec8f379643b56b0d797fb88c0e8f14a812f
ben <ben@192.168.3.8>
2015-12-19 20:03:00 +0100

    Refactor

--- src/main/resources/public/scripts/app/admin/project/edit.controller.js
+++ src/main/resources/public/scripts/app/admin/project/edit.controller.js
@@ -3,6 +3,12 @@
         var editController = function (project) {
             var vm = this;
             vm.project = project;
+            project.$promise.then(function () {
+                vm.project.categories = project.resource("category").query();
+                vm.project.states = project.resource("state").query();
+                vm.project.swimlanes = project.resource("swimlane").query();
+                vm.project.members = project.resource("member").get();
+            });
         };
         editController.$inject = ["project"];
         return editController;
--- src/main/resources/public/scripts/app/admin/project/member/list.controller.js
+++ src/main/resources/public/scripts/app/admin/project/member/list.controller.js
@@ -1,21 +1,7 @@
 (function () {
     define(["angular"], function (angular) {
         var listController = function (projectService, HateoasInterface) {
-            var vm = this;
-            vm.projects = {
-                page: {
-                    number: 0,
-                    size: 15
-                }
-            };
-            vm.projects = projectService.get({page: vm.projects.page.number, size : vm.projects.page.size }, function() {
-                if (vm.projects._embedded) {
-                        angular.forEach(vm.projects._embedded.projectResourceList, function(project) {
-                            project.states = new HateoasInterface(project).resource("state").query();
-                            return project;
-                        });
-                }
-            });
+            
         };
         listController.$inject = ["projectService", "HateoasInterface"];
         return listController;
