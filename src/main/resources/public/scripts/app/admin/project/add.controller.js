be8728113c2a151a52a4dc4438918e73d351b70f
ben <ben@192.168.3.8>
2015-12-20 18:37:33 +0100

    Refactor

--- src/main/resources/public/scripts/app/admin/project/add.controller.js
+++ src/main/resources/public/scripts/app/admin/project/add.controller.js
@@ -1,9 +1,14 @@
 (function () {
     define([], function () {
-        var addController = function () {
+        var addController = function ($uibModalInstance, projects ) {
             var vm = this;
+            vm.submit = function () {
+                projects.resource("self").save(vm.project, function () {
+                    $uibModalInstance.close();
+                });
+            };
         };
-        addController.$inject = [];
+        addController.$inject = ["$uibModalInstance", "projects"];
         return addController;
     });
 })();
