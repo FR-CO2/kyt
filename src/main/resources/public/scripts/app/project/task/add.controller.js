e152fa6cdde39ff1f51d69ae41b485e31768da3f
stanislas GRANEL <dvalin@dvalin.info>
2015-12-22 22:30:33 +0100

    Récupération de la liste des members pour les assignés et backup dans la création d'une nouvelle tache. Modification des paramètres de la fonction de sauvegarde d'une nouvelle tache. Modification du html afin de pouvoir afficher la liste des membres.

--- src/main/resources/public/scripts/app/project/task/add.controller.js
+++ src/main/resources/public/scripts/app/project/task/add.controller.js
@@ -3,9 +3,9 @@
         var addController = function ($uibModalInstance, project) {
             var vm = this;
             vm.categories = project.resource("category").query();
-            vm.members = project.resource("member").get();
+            vm.members = project.resource("member").query();
             vm.submit = function () {
-                project.resource("task").save(null, vm.task, function () {
+                project.resource("task").save(vm.task, function () {
                     $uibModalInstance.close();
                 });
             };
