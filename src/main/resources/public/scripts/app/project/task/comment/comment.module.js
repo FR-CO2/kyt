973f5c8f78c66628f50d91b5c34be87609f012d8
ben <ben@mbp-de-benjamin>
2015-12-23 12:49:01 +0100

    Ajout gestion commentaires

--- src/main/resources/public/scripts/app/project/task/comment/comment.module.js
+++ src/main/resources/public/scripts/app/project/task/comment/comment.module.js
@@ -1,9 +1,11 @@
 (function () {
     define(['angular', "project/task/comment/list.controller",
-        "project/task/comment/add.controller"],
-            function (angular, listController, addController) {
+        "project/task/comment/add.controller",
+        "project/task/comment/reply.controller"],
+            function (angular, listController, addController, replyController) {
                 return angular.module('kanban.project.task.comment', [])
                         .controller("commentListController", listController)
-                        .controller("commentAddController", addController);
+                        .controller("commentAddController", addController)
+                        .controller("commentReplyController", replyController);
             });
 })();
---
+++ src/main/resources/public/scripts/app/project/task/comment/reply.controller.js
@@ -0,0 +1,11 @@
+(function () {
+    define([], function () {
+        var listController = function (scope) {
+            var vm = this;
+            var currentcomment = scope.commentListCtrl.comment;
+            
+        };
+        listController.$inject = ["$uibModal" ,"$scope"];
+        return listController;
+    });
+})();
