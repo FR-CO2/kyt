da044ff65c21d12827089a7c4640ed99585b2fea
ben <ben@mbp-de-benjamin>
2015-12-24 18:57:17 +0100

    Gestion imputation

--- src/main/resources/public/scripts/app/project/task/allocation/list.controller.js
+++ src/main/resources/public/scripts/app/project/task/allocation/list.controller.js
@@ -1,9 +1,27 @@
 (function () {
     define([], function () {
-        var listController = function () {
+        var listController = function ($uibModal, scope) {
             var vm = this;
+            var currenttask = scope.taskCtrl.task;
+            vm.add = function () {
+                var modalInstance = $uibModal.open({
+                    animation: true,
+                    templateUrl: "templates/project/task/allocation/add.html",
+                    controller: "allocationAddController",
+                    controllerAs: "addAllocationCtrl",
+                    resolve: {
+                        task: function () {
+                            return currenttask;
+                        }
+                    },
+                    size: "md"
+                });
+                modalInstance.result.then(function () {
+                    currenttask.allocations = currenttask.resource("allocation").query();
+                });
+            };
         };
-        listController.$inject = [];
+        listController.$inject = ["$uibModal", "$scope"];
         return listController;
     });
 })();
--- src/main/resources/public/scripts/app/project/task/task.controller.js
+++ src/main/resources/public/scripts/app/project/task/task.controller.js
@@ -2,19 +2,22 @@
     define([], function () {
         var taskController = function (project, currenttask, taskAssemblerService) {
             var vm = this;
-            currenttask.$promise.then(function() {
+            currenttask.$promise.then(function () {
                 vm.task = taskAssemblerService(currenttask);
                 vm.task.comments = currenttask.resource("comment").query();
+                vm.task.allocations = currenttask.resource("allocation").query();
             });
-            vm.categories = project.resource("category").query();
-            vm.states = project.resource("state").query();
-            vm.swimlanes = project.resource("swimlane").query();
-            vm.members = project.resource("member").query();
-            vm.selectAssignee = function($item, $model, $label) {
+            project.$promise.then(function () {
+                vm.categories = project.resource("category").query();
+                vm.states = project.resource("state").query();
+                vm.swimlanes = project.resource("swimlane").query();
+                vm.members = project.resource("member").query();
+            });
+            vm.selectAssignee = function ($item, $model, $label) {
                 vm.task.assignee = $model;
             };
-            vm.submit = function() {
-                vm.task.resource("self").save(vm.task, function(){
+            vm.submit = function () {
+                vm.task.resource("self").save(vm.task, function () {
                     history.back();
                 });
             };
