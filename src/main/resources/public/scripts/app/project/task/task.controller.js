2af443bcb48ee027b4ad832205e2153efd9f72df
stanislas GRANEL <dvalin@dvalin.info>
2015-12-27 17:35:43 +0100

    Modification afin d'afficher la date dans le formulaire et lors du submit d'une modification d'une tache.

--- src/main/resources/public/scripts/app/project/task/task.controller.js
+++ src/main/resources/public/scripts/app/project/task/task.controller.js
@@ -1,9 +1,11 @@
 (function () {
     define([], function () {
-        var taskController = function (project, currenttask, taskAssemblerService) {
+        var taskController = function (project, currenttask, taskAssemblerService, dateFilter) {
             var vm = this;
             currenttask.$promise.then(function () {
                 vm.task = taskAssemblerService(currenttask);
+                vm.task.plannedEndingValue = setTimeToValue(currenttask.plannedEnding);
+                vm.task.plannedStartValue = setTimeToValue(currenttask.plannedStart);
                 vm.task.comments = currenttask.resource("comment").query();
                 vm.task.allocations = currenttask.resource("allocation").query();
             });
@@ -18,12 +20,29 @@
                 vm.task.assignee = $model;
             };
             vm.submit = function () {
+                vm.task.plannedStart = setTimeIfTimeNoChange(vm.task.plannedStartValue, vm.task.plannedStart);
+                vm.task.plannedEnding = setTimeIfTimeNoChange(vm.task.plannedEndingValue, vm.task.plannedEnding);
                 vm.task.resource("self").save(vm.task, function () {
                     history.back();
                 });
             };
+            
+            var setTimeToValue = function(timeAngular){
+                var timeToReturn = null;
+                if(timeAngular !== null){
+                    timeToReturn = (dateFilter(new Date(timeAngular), 'yyyy-MM-dd'));
+                }
+                return timeToReturn;
+            };
+            
+            var setTimeIfTimeNoChange = function(timeValue, timeAngular){
+                if(timeAngular === null){
+                   timeAngular = timeValue;
+                }
+                return timeAngular;
+            };
         };
-        taskController.$inject = ["project", "task", "taskAssemblerService"];
+        taskController.$inject = ["project", "task", "taskAssemblerService", "dateFilter"];
         return taskController;
     });
 })();
