7f51a35ef5602a4c2870dcafa0317712b4d5d0ef
ben <ben@192.168.0.10>
2016-01-01 16:39:17 +0100

    Affichage message si saisie d'un utilisateur avec le même nom

--- src/main/resources/public/scripts/app/admin/user/add.controller.js
+++ src/main/resources/public/scripts/app/admin/user/add.controller.js
@@ -6,6 +6,12 @@
             vm.submit = function () {
                 userService.save(vm.user, function () {
                     $uibModalInstance.close();
+                }, function (error) {
+                    if (error.status === 409) {
+                        vm.form.error = "Un utilisateur avec le même nom existe déjà";
+                    } else {
+                        vm.form.error = "Une erreur inattendue s'est produite!";
+                    }
                 });
             };
         };
