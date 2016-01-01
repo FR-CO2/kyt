f00fa755a36f030516c21cfd4abb632662f9c6f1
ben <ben@192.168.0.10>
2016-01-01 10:45:20 +0100

    Le password chiffré admin123 de l'utilisateur admin est présent dans data.sql (penser à rebuilder pour prise en compte lors de l'initialisation)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-MERGE INTO application_user (id,username,password, application_role) KEY(id) values (1, 'admin','$2a$10$MEQtSr5zBFJtffpg5MPFZ.fFf7HHITTP9v4k4EwH3GGq7RUrPGdpy', 0);
+MERGE INTO application_user (id,username,password, application_role) KEY(id) values (1, 'admin','$2a$10$v.Vkaz5PddwFwDMsIOhiwOGeAHuGqZVDhaycH.e9v1iygr6LcDppu', 0);
