7d74a725c7fc2dda3f05895a9e9a59c64ca36e00
ben <ben@mbp-de-benjamin>
2016-01-13 16:38:19 +0100

    Gestion validation entrée

--- src/main/java/org/co2/kanban/rest/error/ExceptionControllerAdvice.java
+++ src/main/java/org/co2/kanban/rest/error/ExceptionControllerAdvice.java
@@ -5,12 +5,14 @@
  */
 package org.co2.kanban.rest.error;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,51 +38,99 @@ public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
     private MessageSource messageSource;
 
     @ExceptionHandler({BusinessException.class})
-    protected ResponseEntity<ErrorMessage> handleBuisinessException(BusinessException e) {
+    protected ResponseEntity handleBuisinessException(BusinessException e) {
+        Error result = new Error(ErrorPhase.INPUT_VALIDATION);
         Locale locale = LocaleContextHolder.getLocale();
         String bundleMessage = messageSource.getMessage(e.getMessageKey(), null, locale);
-        ErrorMessage error = new ErrorMessage(bundleMessage, e.getStatus());
-        return new ResponseEntity<>(error, error.getStatus());
+        result.addMessage(new ErrorMessage(bundleMessage));
+        return new ResponseEntity<>(result, e.getStatus());
+    }
+
+    @ExceptionHandler({DataAccessException.class, SQLException.class})
+    protected ResponseEntity handleDatabaseException(Exception e, WebRequest req) {
+        logger.error("Request: " + req.getContextPath() + " raised " + e);
+        Error result = new Error(ErrorPhase.DATA);
+        Locale locale = LocaleContextHolder.getLocale();
+        String bundleMessage = messageSource.getMessage(DEFAULT_ERROR_MESSAGE_KEY, null, locale);
+        result.addMessage(new ErrorMessage(bundleMessage));
+        return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @Override
     protected ResponseEntity handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         Locale locale = LocaleContextHolder.getLocale();
-        List<ErrorMessage> errors = new ArrayList<>();
-        BindingResult result = ex.getBindingResult();
-        for (ObjectError error : result.getAllErrors()) {
+        Error result = new Error(ErrorPhase.INPUT_VALIDATION);
+        BindingResult validationResult = ex.getBindingResult();
+        for (ObjectError error : validationResult.getGlobalErrors()) {
             String bundleMessage = messageSource.getMessage(error.getCode(), null, locale);
-            errors.add(new ErrorMessage(bundleMessage, HttpStatus.BAD_REQUEST));
+            result.addMessage(new ErrorMessage(bundleMessage));
         }
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+        for (FieldError error : validationResult.getFieldErrors()) {
+            String bundleMessage = messageSource.getMessage(error.getCode(), null, locale);
+            result.addMessage(new ErrorMessage(bundleMessage, error.getField()));
+        }
+        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({Exception.class})
-    protected ResponseEntity<ErrorMessage> handleUnexpectedException(Exception e, WebRequest req) {
+    protected ResponseEntity handleUnexpectedException(Exception e, WebRequest req) {
         logger.error("Request: " + req.getContextPath() + " raised " + e);
+        Error result = new Error(ErrorPhase.UNKNOW);
         Locale locale = LocaleContextHolder.getLocale();
         String bundleMessage = messageSource.getMessage(DEFAULT_ERROR_MESSAGE_KEY, null, locale);
-        ErrorMessage error = new ErrorMessage(bundleMessage, HttpStatus.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<>(error, error.getStatus());
+        result.addMessage(new ErrorMessage(bundleMessage));
+        return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public enum ErrorPhase {
+
+        INPUT_VALIDATION, DATA, UNKNOW
+    }
+
+    public static final class Error {
+
+        private final ErrorPhase phase;
+
+        private final List<ErrorMessage> messages = new ArrayList<>();
+
+        public Error(ErrorPhase phase) {
+            this.phase = phase;
+        }
+
+        public ErrorPhase getPhase() {
+            return phase;
+        }
+
+        public List<ErrorMessage> getMessages() {
+            return this.messages;
+        }
+
+        public void addMessage(ErrorMessage msg) {
+            this.messages.add(msg);
+        }
     }
 
     public static final class ErrorMessage {
 
         private final String message;
 
-        private final HttpStatus status;
+        private String fieldName;
 
-        public ErrorMessage(String message, HttpStatus status) {
+        public ErrorMessage(String message) {
             this.message = message;
-            this.status = status;
+        }
+
+        public ErrorMessage(String message, String fieldName) {
+            this.message = message;
+            this.fieldName = fieldName;
         }
 
         public String getMessage() {
             return message;
         }
 
-        public HttpStatus getStatus() {
-            return status;
+        public String getFieldName() {
+            return fieldName;
         }
 
     }
