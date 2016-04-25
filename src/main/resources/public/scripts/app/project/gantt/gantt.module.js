0fca4426481f0b33710d557cf05614c9717227eb
courtib <>
2016-04-25 19:32:59 +0200

    Ajout diagramme de gantt

--- src/main/resources/public/scripts/app/project/gantt/gantt.module.js
+++ src/main/resources/public/scripts/app/project/gantt/gantt.module.js
@@ -1,12 +1,6 @@
 var ganttController = require("./gantt.controller");
-module.exports = angular.module("kanban.project.gantt", ["gantt", 'gantt.sortable',
-    'gantt.movable',
-    'gantt.drawtask',
-    'gantt.tooltips',
-    'gantt.bounds',
-    'gantt.progress',
-    'gantt.table',
-    'gantt.tree',
-    'gantt.groups',
-    'gantt.resizeSensor'])
+module.exports = angular.module("kanban.project.gantt", 
+    ["gantt", 'gantt.sortable', 'gantt.movable', 'gantt.overlap', 'gantt.dependencies',
+    'gantt.drawtask', 'gantt.tooltips', 'gantt.bounds', 'gantt.progress',
+    'gantt.table', 'gantt.tree', 'gantt.groups', 'gantt.resizeSensor'])
         .controller("ganttController", ganttController);
