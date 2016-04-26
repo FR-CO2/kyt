bd1369986385220754e4440a58b19c2d3e4ddb1c
granels <granels@FR-L2669550.groupinfra.com>
2016-04-26 15:44:48 +0200

    Modification afin de permettre de configurer les imputations pour le projet. En cours de dev.

--- src/main/resources/public/scripts/app/admin/project/allocation/allocation.module.js
+++ src/main/resources/public/scripts/app/admin/project/allocation/allocation.module.js
@@ -1,5 +1,3 @@
 var listController = require("./list.controller");
-var addController = require("./add.controller");
 module.exports = angular.module('kanban.admin.project.allocation', [])
-        .controller("listAllocationAdminController", listController)
-        .controller("addAllocationAdminController", addController);
+        .controller("listAllocationAdminController", listController);
